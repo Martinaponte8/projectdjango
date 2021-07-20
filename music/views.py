27971570d179fcd62298f2845f3b84088b5c04db
David Veron <davidcuevas15@hotmail.com>
2021-07-20 15:49:47 -0400

    Se aplico cambios en la funcion index

--- music/views.py
+++ music/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>This is the music app home page </h1>")
+    return HttpResponse("<h1>Este es una aplicación de musica </h1>")
